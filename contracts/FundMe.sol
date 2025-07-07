35833c8a26c842518966937065eb971d66d34be6
ruochen <wangrui@ruochen.email>
2025-07-07 22:43:16 +0800

    chai test

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,7 +9,7 @@ contract FundMe {
 
     uint256 constant MINIMUM_VALUE = 100 * 10 ** 18; // USD
 
-    AggregatorV3Interface internal dataFeed;
+    AggregatorV3Interface public dataFeed;
 
     // 投资目标值
     uint256 constant TARGET = 1000 * 10 ** 18;
