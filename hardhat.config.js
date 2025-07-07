a086986f1d1b447230619ad7f8dabf33ff41ee8f
ruochen <wangrui@ruochen.email>
2025-07-07 20:43:24 +0800

    contract interaction script

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("@chainlink/env-enc").config()
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -12,15 +13,17 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      // alchemy
+      // alchemy app url
       url: SEPOLIA_URL,
       // metamask private key
-      accounts: [PRIVATE_KEY],
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_1],
+      // chainlist.org
       chainId: 11155111
     }
   },
   etherscan: {
     apiKey: {
+      // for verify
       sepolia: ETHERSCAN_API_KEY
     }
   }
--- scripts/DeployFundMe.js
+++ scripts/DeployFundMe.js
@@ -1,11 +1,5 @@
 // import ethers
 // create main function
-    // init 2 accounts
-    // fund contract with first account
-    // check balance of contract
-    // fund contract with second account
-    // check balance of contract
-    // check mapping funders2Amount
 // execute main function
 
 const { ethers } = require("hardhat");
@@ -16,7 +10,7 @@ async function main() {
         
     console.log('contract deploying...')
     // deploy contract from factory
-    const fundMe = await fundMeFactory.deploy(10)
+    const fundMe = await fundMeFactory.deploy(300)
     await fundMe.waitForDeployment()
     console.log(`contract has been deployed successfully, contract address is ${fundMe.target}`)
 
@@ -24,10 +18,35 @@ async function main() {
     if (hre.network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
         console.log("waiting for 5 confirmations...")
         await fundMe.deploymentTransaction().wait(5)
-        await verifyFundMe(fundMe.target, [10])
+        await verifyFundMe(fundMe.target, [300])
     } else {
         console.log("verification skipped...")
     }
+
+    // init 2 accounts
+    const [firstAccount, secondAccount] = await ethers.getSigners()
+    // fund contract with first account
+    const fundTx = await fundMe.fund({value: ethers.parseEther("0.5")})
+    // wait for complete
+    await fundTx.wait()
+
+    // check balance of contract
+    const balanceOfContract = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContract}`)
+
+    // fund contract with second account
+    const fundTxWithSecondAccount = await fundMe.connect(secondAccount).fund({value: ethers.parseEther("0.5")})
+    await fundTxWithSecondAccount.wait()
+
+    // check balance of contract
+    const balanceOfContractAfterSecond = await ethers.provider.getBalance(fundMe.target)
+    console.log(`Balance of the contract is ${balanceOfContractAfterSecond}`)
+
+    // check mapping funder2Amount
+    const firstAccountBalanceInFundMe = await fundMe.funder2Amount(firstAccount.address)
+    const secondAccountBalanceInFundMe = await fundMe.funder2Amount(secondAccount.address)
+    console.log(`Balance of first account ${firstAccount.address} is ${firstAccountBalanceInFundMe}`)
+    console.log(`Balance of first account ${secondAccount.address} is ${secondAccountBalanceInFundMe}`)
 }
 
 async function verifyFundMe(fundMeAddr, args) {
