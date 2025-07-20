a42989d5116d0fe2a2c59df7a3a556fe1fda7e34
ruochen <wangrui@ruochen.email>
2025-07-20 14:45:48 +0800

    chore: move verify function to utils directory

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -9,46 +9,48 @@
 //     console.log('this is a deploy function')
 // }
 
-const { network } = require("hardhat");
+const { network } = require('hardhat')
 const {
-  DEVELOPMENT_CHAINS,
-  NETWORK_CONFIG,
-  LOCK_TIME,
-  CONFIRMATIONS,
-} = require("../helper-hardhat-config");
+	DEVELOPMENT_CHAINS,
+	NETWORK_CONFIG,
+	LOCK_TIME,
+	CONFIRMATIONS,
+} = require('../helper-hardhat-config')
+const { verify } = require('../utils/verify')
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { firstAccount } = await getNamedAccounts();
-  const { deploy } = deployments;
+	const { firstAccount } = await getNamedAccounts()
+	const { deploy } = deployments
 
-  let dataFeedAddr, confirmations;
-  if (DEVELOPMENT_CHAINS.includes(network.name)) {
-    const mockV3Aggregator = await deployments.get("MockV3Aggregator");
-    dataFeedAddr = mockV3Aggregator.address;
-    confirmations = 0;
-  } else {
-    dataFeedAddr = NETWORK_CONFIG[network.config.chainId].ETH_USD_DATA_FEED;
-    confirmations = CONFIRMATIONS;
-  }
+	let dataFeedAddr, confirmations
+	if (DEVELOPMENT_CHAINS.includes(network.name)) {
+		const mockV3Aggregator = await deployments.get('MockV3Aggregator')
+		dataFeedAddr = mockV3Aggregator.address
+		confirmations = 0
+	} else {
+		dataFeedAddr = NETWORK_CONFIG[network.config.chainId].ETH_USD_DATA_FEED
+		confirmations = CONFIRMATIONS || 1
+	}
 
-  const fundMe = await deploy("FundMe", {
-    from: firstAccount,
-    args: [LOCK_TIME, dataFeedAddr],
-    log: true,
-    // wait for block
-    waitConfirmations: confirmations,
-  });
-  // remove deployment directory or add --reset flag if you redeploy contract
+	const fundMe = await deploy('FundMe', {
+		from: firstAccount,
+		args: [LOCK_TIME, dataFeedAddr],
+		log: true,
+		// wait for block
+		waitConfirmations: confirmations,
+	})
+	// remove deployment directory or add --reset flag if you redeploy contract
 
-  // verify
-  if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
-    await hre.run("verify:verify", {
-      address: fundMe.address,
-      constructorArguments: [LOCK_TIME, dataFeedAddr],
-    });
-  } else {
-    console.log("verification skipped...");
-  }
-};
+	// verify
+	if (
+		!DEVELOPMENT_CHAINS.includes(network.name) &&
+		process.env.ETHERSCAN_API_KEY
+	) {
+		const args = [LOCK_TIME, dataFeedAddr]
+		await verify(fundMe.address, args)
+	} else {
+		console.log('verification skipped...')
+	}
+}
 
-module.exports.tags = ["all", "fundme"];
+module.exports.tags = ['all', 'fundme']
